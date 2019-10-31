13354e59282ae5ec4a934d03a09c38c8867d4ba7
Jordan Allen <land.of.jordan@gmail.com>
2019-10-31 13:39:24 -0700

    updated users.js with proper routes

--- family-date-night/routes/users.js
+++ family-date-night/routes/users.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
 var models = require('../models'); 
 
 /* GET users listing. */
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
@@ -28,3 +34,39 @@ router.post('/signup', function(req, res, next) {
       }
     });
 });
+
+router.get('/profile/:id', function (req, res, next) {
+  models.users
+    .findByPk(parseInt(req.params.id))
+    .then(user => {
+      if (user) {
+        res.render('profile', {
+          FirstName: user.FirstName,
+          LastName: user.LastName,
+          Email: user.Email,
+          Username: user.Username
+        });
+      } else {
+        res.send('User not found');
+      }
+    });
+  });
+
+router.post('/login', function(req, res, next) {
+  models.users
+    .findOne({
+      where: {
+        Username: req.body.username,
+        Password: req.body.password
+      }
+    })
+    .then(user => {
+      if (user) {
+        res.send('Login succeeded!');
+      } else {
+        res.send('Invalid login!');
+      }
+    });
+});
+
+module.exports = router;

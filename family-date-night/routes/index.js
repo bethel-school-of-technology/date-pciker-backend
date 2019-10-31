f84237f1f0302825815a168b9d689af5cfb71474
Jordan Allen <land.of.jordan@gmail.com>
2019-10-31 12:00:54 -0700

    added signup hbs and users js get and post routes

--- family-date-night/routes/index.js
+++ family-date-night/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'sakila'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- family-date-night/routes/users.js
+++ family-date-night/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models'); 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
 });
 
-module.exports = router;
+router.post('/signup', function(req, res, next) {
+  models.users
+    .findOrCreate({
+      where: {
+        Username: req.body.username
+      },
+      defaults: {
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName,
+        Email: req.body.email,
+        Password: req.body.password
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        res.send('User successfully created');
+      } else {
+        res.send('This user already exists');
+      }
+    });
+});

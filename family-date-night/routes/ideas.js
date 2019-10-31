4d090194646b3511438277f492139cf9402a2f15
Jordan Allen <land.of.jordan@gmail.com>
2019-10-31 13:22:52 -0700

    added new views and routes

---
+++ family-date-night/routes/ideas.js
@@ -0,0 +1,30 @@
+// var express = require('express');
+// var router = express.Router();
+// var models = require('../models'); 
+
+// /* GET users listing. */
+// router.get('/signup', function(req, res, next) {
+//   res.render('signup');
+// });
+
+// router.post('/signup', function(req, res, next) {
+//   models.users
+//     .findOrCreate({
+//       where: {
+//         Username: req.body.username
+//       },
+//       defaults: {
+//         FirstName: req.body.firstName,
+//         LastName: req.body.lastName,
+//         Email: req.body.email,
+//         Password: req.body.password
+//       }
+//     })
+//     .spread(function(result, created) {
+//       if (created) {
+//         res.send('User successfully created');
+//       } else {
+//         res.send('This user already exists');
+//       }
+//     });
+// });

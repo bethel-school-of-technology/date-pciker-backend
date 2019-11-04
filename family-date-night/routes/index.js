1430d6411aaf1e7b709da6e768ea11d6f94b4061
Jordan Allen <land.of.jordan@gmail.com>
2019-11-04 11:44:00 -0800

    ican't remember

--- family-date-night/routes/index.js
+++ family-date-night/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
-var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -9,6 +8,15 @@ var connection = mysql.createConnection({
   password: 'Password1!',
   database: 'sakila'
 });
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Yay! You are connected to the database!');
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

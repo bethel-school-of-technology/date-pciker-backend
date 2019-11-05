009a598cfa56e5007dce794d8f524b12e5fc7bd6
Seth Rhodes <bstsethshow@gmail.com>
2019-11-04 17:56:51 -0800

    using new db schema familydate2

---
+++ express_clean_build/models/ideas.js
@@ -0,0 +1,33 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('ideas', {
+    UserId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    Topic: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    Description: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    IdeasId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true
+    }
+  }, {
+    tableName: 'ideas'
+  });
+};

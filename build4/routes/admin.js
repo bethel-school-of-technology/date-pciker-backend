526d7725175b2a1b26fb0e70a655f27e30dd2f98
Seth Rhodes <bstsethshow@gmail.com>
2019-11-06 09:27:31 -0800

    added tokens to login and signup

---
+++ build4/routes/admin.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+//NEEDED: SECURE GET, POST, PUT, & DELETE ADMIN ROUTES
+
+module.exports = router;

ff0444b0d2d2ecd67136ee58255009c11697bbfb
Seth Rhodes <bstsethshow@gmail.com>
2019-11-14 18:51:43 -0800

    re-added ideas_users model

---
+++ build4/models/ideas_users.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const ideas_users = sequelize.define('ideas_users', {
+    UserId: DataTypes.INTEGER,
+    IdeaId: DataTypes.INTEGER
+  }, {});
+  ideas_users.associate = function(models) {
+    // associations can be defined here
+  };
+  return ideas_users;
+};
